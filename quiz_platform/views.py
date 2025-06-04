26e2fe66e0e484764a50534b138d081c66135850
llanunciado <llanunciado@up.edu.ph>
2025-06-04 19:18:42 +0800

    fixed My Quizzes list

--- quiz_platform/views.py
+++ quiz_platform/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Quiz
 
 def home_view(request):
     return HttpResponse("""
@@ -12,3 +14,17 @@ def home_view(request):
         </body>
     </html>
     """)
+
+@login_required
+def user_quizzes(request):
+    quizzes = Quiz.objects.filter(owner=request.user)
+    data = [
+        {
+            'id': quiz.id,
+            'title': quiz.title,
+            'created_at': quiz.created_at.strftime('%Y-%m-%d'),
+        }
+        for quiz in quizzes
+    ]
+    return JsonResponse({'quizzes': data})
+
